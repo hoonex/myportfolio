e5879fa87e52d808229eaa0c98eb740d48eb174d
hoonex <163626004+hoonex@users.noreply.github.com>
2026-09-15 11:45:45 +0900

    chore: bump Liquid Piano cache revision

--- journal-route-loader.js
+++ journal-route-loader.js
@@ -7,7 +7,7 @@
   const fullEditorialRoutes = new Set(['/lab', '/post/glass', '/post/sloar', '/post/motion']);
 
   const route = () => (location.hash.slice(1) || '/').split('?')[0];
-  const DEPLOY_REV = 'SITE46-LIQUIDPIANO1-20260915-1110';
+  const DEPLOY_REV = 'SITE47-LIQUIDPIANO2-WEBGL-20260915-1135';
   const assetUrl = rel => {
     const url = new URL(`./${rel}`, document.baseURI);
     url.searchParams.set('v', DEPLOY_REV);
